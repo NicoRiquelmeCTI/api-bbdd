40fcf8f385a1db037e5cbb0e17ea964993ee582b
NicoRiquelmeCTI <nariquelme@uc.cl>
2019-12-26 07:04:50 -0300

    consulta buscar por nombre proyecto

--- main.py
+++ main.py
@@ -1,17 +1,60 @@
 ﻿from flask import Flask, render_template, request, abort, json
 from pymongo import MongoClient
 import pandas as pd
-import matplotlib.pyplot as plt
 import os
+import atexit
+import subprocess
 
+#Instanciar las llaves del documento
+D_KEYS = ["id", "content", "metadata"]
+METADATA_KEYS = ["time", "sender", "receiver"]
 USER_KEYS = ['name', 'last_name', 'occupation', 'follows', 'age']
 
-# El cliente se levanta en la URL de la wiki
-URL = "mongodb://grupo90:grupo90@gray.ing.puc.cl/grupo90"
-client = MongoClient(URL)
+#Levantar servidor de Mongo
+mongod = subprocess.Popen("mongod", stdout=subprocess.DEVNULL)
 
-# Iniciamos la aplicación de flask
+uri = "mongodb://grupo58:grupo58@gray.ing.puc.cl/grupo58"
+# La uri 'estándar' es "mongodb://user:password@ip/database"
+atexit.register(mongod.kill)
+client = MongoClient(uri)
+db = client.get_database()
+
+#Seleccionar colección de correos
+correos = db.correos
+
+#Instanciar aplicación de Flask
 app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>Bienvenido</h1>"
+
+@app.route("/message/<string:mid>")
+def get_message(mid):
+    mensajes = list(correos.find(mid))
+    return json.jsonify(mensajes)
+
+@app.route("/message/<string:id>", methods=['DELETE'])
+def delete_message(id):
+    correos.delete_one({"id": id})
+    mensaje = f"Correo id {id} ha sido eliminado"
+    return json.jsonify({'result': 'success', 'msn': mensaje})
+
+@app.route("/message/", methods=['POST'])
+def new_message():
+    return
+
+@app.route("/messages/project-search<string:receiver>")
+def project_search_message(receiver):
+    correos.createIndex({"metadata.sender":"text"})
+    encontrados_sender = entidades.find({$text: {$search: "\"receiver\""}},{"metadata.sender":1})
+    encontrados_rec = entidades.find({$text: {$search: "\"receiver\""}},{"metadata.receiver":1})
+    encontrados = encontrados_sender + encontrados_rec
+    return json.jsonify(encontrados)
+
+@app.route("/messages/content-search<string:content>")
+def content_search_message(content):
+    return
 
 @app.route("/")
 def home():
